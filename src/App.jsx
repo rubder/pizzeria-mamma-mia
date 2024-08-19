c7d4cc9e5bd708723f5e6aaf6f09c18900d8be47
Ruben Millan <165565025+rubder@users.noreply.github.com>
2024-08-19 03:12:43 -0400

    login y register
    creacion de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
-
-import React from 'react'
+import React, { useState } from 'react';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import Footer from './components/Footer';
+import RegisterPage from './components/RegisterPage';
+import LoginPage from './components/LoginPage';
 
 function App() {
+  const [currentPage, setCurrentPage] = useState('home'); // Estado para controlar la página actual
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'home':
+        return <Home />;
+      case 'register':
+        return <RegisterPage />;
+      case 'login':
+        return <LoginPage />;
+      default:
+        return <Home />;
+    }
+  };
+
   return (
     <div>
-      <Navbar />
-      <Home />
+      <Navbar setCurrentPage={setCurrentPage} /> {/* Pasamos setCurrentPage como prop */}
+      {renderPage()}
       <Footer />
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,26 +1,27 @@
-const Navbar = () => {
-    const total = 25000;
-    const token = false;
-  
-    return (
-      <nav>
-        <h2>Pizzería Mamma Mía</h2>
-        <button>🍕 Home</button>
-        {token ? (
-          <>
-            <button>🔓 Profile</button>
-            <button>🔒 Logout</button>
-          </>
-        ) : (
-          <>
-            <button>🔐 Login</button>
-            <button>🔐 Register</button>
-          </>
-        )}
-        <button className="navbar-button navbar-total">🛒 Total: ${total.toLocaleString()}</button>
-      </nav>
-    );
-  };
-  
-  export default Navbar;
+const Navbar = ({ setCurrentPage }) => {
+  const total = 25000;
+  const token = false;
+
+  return (
+    <nav>
+      <h2>Pizzería Mamma Mía</h2>
+      <button onClick={() => setCurrentPage('home')}>🍕 Home</button>
+      {token ? (
+        <>
+          <button onClick={() => setCurrentPage('profile')}>🔓 Profile</button>
+          <button>🔒 Logout</button>
+        </>
+      ) : (
+        <>
+          <button onClick={() => setCurrentPage('login')}>🔐 Login</button>
+          <button onClick={() => setCurrentPage('register')}>🔐 Register</button>
+        </>
+      )}
+      <button className="navbar-button navbar-total">🛒 Total: ${total.toLocaleString()}</button>
+    </nav>
+  );
+};
+
+export default Navbar;
+
   
